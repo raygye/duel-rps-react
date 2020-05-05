e5d967819e79c0ea72638da436a0a01e34309bbb
yeG10 <garyye2007@gmail.com>
2020-05-05 16:26:03 -0400

    check for login persistence

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,17 @@ import Login from './Login';
 import Signup from './Signup';
 import duel from './imgs/duel.png';
 export default class Home extends Component {
+    componentDidMount() {
+        fetch('http://localhost:5000/users/checkLogged', {
+            method: 'GET'
+        }).then(
+            function(response) {
+                response.text().then(function(data) {
+                    console.log(data);
+                });
+            })
+    }
+
     render() {
         return(
             <div className="container">
